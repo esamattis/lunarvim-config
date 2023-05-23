143f89b5401fbbdb2c9114c4190e5d7ed4956182
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-05-23 18:10:57 +0300

    asdf

--- lua/user/buffer_toggle.lua
+++ lua/user/buffer_toggle.lua
@@ -3,6 +3,10 @@ local current_buffer = nil
 
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
     callback = function()
+        if vim.bo.buftype == "terminal" then
+            return
+        end
+
         if current_buffer ~= nil then
             previous_buffer = current_buffer
         end
