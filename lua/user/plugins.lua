4d3080b9840ab9f5f93a301db15e254f53510d2c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-05-27 15:30:29 +0300

    Add treesitter context

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -11,8 +11,38 @@ return {
     { "olimorris/onedarkpro.nvim" },
     { "rmehri01/onenord.nvim" },
     { "shaunsingh/nord.nvim" },
-    { "nvim-pack/nvim-spectre",     lazy = true },
-    { "almo7aya/openingh.nvim",     cmd = { "OpenInGHFile" } },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+
+        config = function()
+            require("treesitter-context").setup {
+                enable = true,   -- Enable this plugin (Can be enabled/disabled later via commands)
+                throttle = true, -- Throttles plugin updates (may improve performance)
+                max_lines = 3,   -- How many lines the window should span. Values <= 0 mean no limit.
+                patterns = {
+                    -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+                    -- For all filetypes
+                    -- Note that setting an entry here replaces all other patterns for this entry.
+                    -- By setting the 'default' entry below, you can control which nodes you want to
+                    -- appear in the context window.
+                    default = {
+                        'class',
+                        'function',
+                        'method',
+                        'test',
+                    },
+                },
+            }
+        end
+    },
+    { "nvim-pack/nvim-spectre", lazy = true },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    { "almo7aya/openingh.nvim", cmd = { "OpenInGHFile" } },
     -- {
     --     "catppuccin/nvim",
     --     name = "catppuccin"
