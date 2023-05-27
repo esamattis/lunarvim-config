8e35139683de52c45db6424fda82d390c8d68a70
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-05-27 15:30:18 +0300

    Add neovide support

--- config.lua
+++ config.lua
@@ -27,6 +27,30 @@ if os.getenv("ITERM_PROFILE") == "Light" then
 end
 
 
+if vim.g.neovide then
+    lvim.colorscheme = "tokyonight"
+    vim.opt.linespace = 0
+    vim.g.neovide_scale_factor = 1.0
+    vim.o.guifont = "SauceCodePro Nerd Font:h14"
+    vim.g.neovide_remember_window_size = true
+    vim.g.neovide_input_macos_alt_is_meta = true
+
+
+    vim.g.neovide_input_use_logo = 1            -- enable use of the logo (cmd) key
+    vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
+    vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
+    vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
+    vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
+    vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
+    vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+
 
 
 -- always exit vim no matter what with <m-x>
@@ -104,3 +128,13 @@ vim.keymap.set("n", "<space><space>", "*N")
 lvim.builtin.which_key.mappings["j"] = {
     "'", "Jump to mark"
 }
+
+
+if vim.fn.argc() == 0 then
+    vim.defer_fn(
+        function()
+            vim.cmd('Telescope projects')
+        end,
+        0
+    )
+end
