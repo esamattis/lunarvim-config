f3a3ccacc5ea4efb7411320a359b2bf6d4753b3a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-05-28 11:36:39 +0300

    Fix paste

--- config.lua
+++ config.lua
@@ -34,22 +34,26 @@ if vim.g.neovide then
     vim.g.neovide_scale_factor = 1.0
     vim.o.guifont = "SauceCodePro Nerd Font:h14"
     vim.g.neovide_remember_window_size = true
+    vim.g.neovide_cursor_vfx_mode = "pixiedust"
     -- vim.g.neovide_input_macos_alt_is_meta = true
 
 
-    vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
-    vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
-    vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
-    vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
-    vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
-    vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
-    vim.g.neovide_cursor_vfx_mode = "pixiedust"
+    vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+    vim.keymap.set('v', '<D-c>', '"+ygv')  -- Copy
+    vim.keymap.set('n', '<D-v>', '"+P')    -- Paste normal mode
+    vim.keymap.set('v', '<D-v>', '"+P')    -- Paste visual mode
+    vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+    -- vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli', { noremap = true, silent = true }) -- Paste insert mode
+    vim.keymap.set('i', '<D-v>', function()
+        vim.cmd("stopinsert")
+        vim.cmd('normal! "+P')
+    end, { noremap = true, silent = true }) -- Paste insert mode
 end
 
-vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('!', '<D-v>', '"+P', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('t', '<D-v>', '"+P', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('v', '<D-v>', '"+P', { noremap = true, silent = true })
 
 
 
