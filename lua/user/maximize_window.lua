fd62e587e80f5545d53551941e9d77f254e4dae6
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-05-30 10:26:58 +0300

    maximize++

--- lua/user/maximize_window.lua
+++ lua/user/maximize_window.lua
@@ -1,15 +1,23 @@
 local m = {};
 
+
 function m.is_maximized()
     return vim.fn.tabpagenr("$") ~= 1
 end
 
 function m.maximize()
-    if not m.is_maximized() then
-        vim.cmd("tab split")
-        if vim.bo.buftype == "terminal" then
-            vim.api.nvim_command("startinsert")
-        end
+    if m.is_maximized() then
+        return
+    end
+
+    if #vim.api.nvim_list_wins() == 1 then
+        print("Can't maximize with only one window")
+        return
+    end
+
+    vim.cmd("tab split")
+    if vim.bo.buftype == "terminal" then
+        vim.api.nvim_command("startinsert")
     end
 end
 
